14161a3b66c24a51f6018e764b2430df60d7302b
Chris Elion <6877802+chriselion@users.noreply.github.com>
2024-09-23 21:18:30 -0700

    protobufs

--- tests/example/app.py
+++ tests/example/app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from tests.example.proto.hello_world_pb2 import HelloRequest
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+    HelloRequest()
     return "<p>Hello, World!</p>"
