a94c02c4328dc3cd1ee277ad4c17da38ab609355
Chris Elion <6877802+chriselion@users.noreply.github.com>
2024-09-23 21:53:38 -0700

    WIP some registration

--- tests/example/app.py
+++ tests/example/app.py
@@ -1,10 +1,35 @@
 from flask import Flask
-from tests.example.proto.hello_world_pb2 import HelloRequest
+import functools
+from tests.example.proto.hello_world_pb2 import HelloRequest, HelloReply
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    HelloRequest()
+def hello_world() -> str:
     return "<p>Hello, World!</p>"
+
+def rpc(route):
+    def rpc_decorator(func):
+        @functools.wraps(func)
+        def rpc_inner(*args, **kwargs):
+            # Before
+            ret = func(*args, **kwargs)
+            # After
+            return ret
+        return rpc_inner
+
+    #print(app.url_map)
+    print(f"Registering {route=}")
+    app.add_url_rule(route, view_func=rpc_decorator)
+    #print(app.url_map)
+
+    return rpc_decorator
+
+@rpc("/Greeter/SayHello/")
+def rpc_impl(req: HelloRequest) -> HelloReply:
+    message = "Hello, " + (req.name or "world")
+    resp = HelloReply(message=message)
+    return resp
+
+print(app.url_map)
--- tests/unit/test_bridge.py
+++ tests/unit/test_bridge.py
@@ -5,3 +5,8 @@ def test_app():
     client = app.test_client()
     resp = client.get("/")
     assert resp.status_code == 200
+
+def test_hello_rpc():
+    client = app.test_client()
+    resp = client.get("/Greeter/SayHello/")
+    assert resp.status_code == 200
