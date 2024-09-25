6268a364fc463e56493f23386c4faf66410cbcd9
Chris Elion <6877802+chriselion@users.noreply.github.com>
2024-09-25 10:38:52 -0700

    Bridge class

--- tests/example/app.py
+++ tests/example/app.py
@@ -18,58 +18,69 @@ CONTENT_TYPE_HEADER = "Content-Type"
 PROTOBUF_CONTENT_TYPE = "application/protobuf"
 
 
-def rpc(service_module, service_name, method_name):
-    route = f"/{service_name}/{method_name}/"
+class Bridge:
+    def __init__(self, service_module, service_name):
+        self.service_name = service_name
+        self.module_descriptor = service_module.DESCRIPTOR
+        self.service_descriptor = self.module_descriptor.services_by_name[service_name]
 
-    module_descriptor = service_module.DESCRIPTOR
-    service_descriptor = module_descriptor.services_by_name[service_name]
-    method_descriptor = service_descriptor.methods_by_name[method_name]
-    # TODO compare against function's input and output type annotations
-    # TODO ^ at type-check time
-    input_type = method_descriptor.input_type._concrete_class
-    output_type = method_descriptor.input_type._concrete_class  # noqa
+    def rpc(self, method_name: str | None = None):
+        def rpc_decorator(func):
+            nonlocal method_name
+            method_name = method_name or func.__name__
 
-    def rpc_decorator(func):
-        @functools.wraps(func)
-        def rpc_inner(*args, **kwargs):
+            @functools.wraps(func)
+            def rpc_inner(*args, **kwargs):
 
-            input_message = input_type()
-            # Read the request from either the json body, or directly as bytes
-            try:
-                # TODO parse options
-                json_format.ParseDict(
-                    request.json, input_message, ignore_unknown_fields=True
-                )
-                is_body_proto = False
-            except UnsupportedMediaType:
-                is_body_proto = True
-                input_message.ParseFromString(request.data)
-            # TODO handle parse errors, unknown fields etc as 4xx
+                method_descriptor = self.service_descriptor.methods_by_name[method_name]
+                # TODO compare against function's input and output type annotations
+                # TODO ^ at type-check time
+                input_type = method_descriptor.input_type._concrete_class
+                output_type = method_descriptor.input_type._concrete_class  # noqa
 
-            # Make the function call
-            ret = func(input_message)
+                input_message = input_type()
+                # Read the request from either the json body, or directly as bytes
+                try:
+                    # TODO parse options
+                    json_format.ParseDict(
+                        request.json, input_message, ignore_unknown_fields=True
+                    )
+                    is_body_proto = False
+                except UnsupportedMediaType:
+                    is_body_proto = True
+                    input_message.ParseFromString(request.data)
+                # TODO handle parse errors, unknown fields etc as 4xx
 
-            # Return the response in the same format
-            if is_body_proto:
-                response_bytes = ret.SerializeToString()
-                return Response(
-                    response_bytes, 200, {CONTENT_TYPE_HEADER: PROTOBUF_CONTENT_TYPE}
-                )
-            else:
-                # TODO parse options, handle int64
-                response_json = json_format.MessageToDict(ret)
-                return response_json
+                # Make the function call
+                ret = func(input_message)
 
-        print(f"Registering {route=}")
-        app.add_url_rule(route, view_func=rpc_inner, methods=["POST"])
+                # Return the response in the same format
+                if is_body_proto:
+                    response_bytes = ret.SerializeToString()
+                    return Response(
+                        response_bytes,
+                        200,
+                        {CONTENT_TYPE_HEADER: PROTOBUF_CONTENT_TYPE},
+                    )
+                else:
+                    # TODO parse options, handle int64
+                    response_json = json_format.MessageToDict(ret)
+                    return response_json
 
-        return rpc_inner
+            route = f"/{self.service_name}/{method_name}/"
+            print(f"Registering {route=}")
+            app.add_url_rule(route, view_func=rpc_inner, methods=["POST"])
 
-    return rpc_decorator
+            return rpc_inner
+
+        return rpc_decorator
 
 
-@rpc(hello_world_pb2, "Greeter", "SayHello")
-def rpc_impl(req: HelloRequest) -> HelloReply:
+bridge = Bridge(hello_world_pb2, "Greeter")
+
+
+@bridge.rpc("SayHello")
+def SayHello(req: HelloRequest) -> HelloReply:
     message = "Hello, " + (req.name or "world")
     resp = HelloReply(message=message)
     return resp
